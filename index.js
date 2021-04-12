09bba9a241e1ddecd4b304f82aed466af1bf42bf
Marquis <Marquis.jayshaun.jarman@gmail.com>
2021-04-12 08:56:19 -0400

    hw push

---
+++ index.js
@@ -0,0 +1,45 @@
+alert("Hello World!"); 
+
+// make the image invisible
+
+//move the image from top to bottom
+
+// dont start with capitals
+
+//Variables
+
+var myvariableGoose;
+
+myvariableGoose= 10;
+
+var myvar2;
+myvar2 = hello world;
+myvar2= 5;
+
+var customerAge = 32;
+
+var myVar3 = 40;
+
+//Math operators
+
+var myVar = 5
+
+myVar + 10 = 15
+
+myVar + myVar = 10
+
+5 +"hello" = "5hello"
+
+"hello" +  "david" = "hello david"
+
+//Operator Shorthand
+
+myVar += 5
+
+myVar -= 20
+
+myVar /= 2
+
+myVar *= 25
+
+myVar ++
